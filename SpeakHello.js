56f961806787da275ea867e9041cc6630d342774
diptiLll <164995573+diptiLll@users.noreply.github.com>
2024-03-29 16:00:10 +0530

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,11 @@
+
+
+(function(window) {
+	var speakWord = "Hey";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
---
+++ script.js
@@ -0,0 +1,21 @@
+
+
+(function () {
+
+    var names = ["Savannah", "Luna", "Isabella", "Harper", "Jade", "Amelia"];
+    
+    for (var i = 0; i < names.length; i++) {
+      var firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        byeSpeaker(names[i]);
+      }
+      else {
+        helloSpeaker(names[i]);
+      }
+    }
+    
+    })();
+    
+    
+    
